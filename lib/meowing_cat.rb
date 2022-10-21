e2cdbac6513291759cbbc849020f111d57985e28
Kelvin M Matheka <beingmatheka@gmail.com>
2022-10-21 22:51:36 +0300

    Done with assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,20 @@
 ## code your solution here. 
+
+class Cat
+
+  attr_accessor :name
+  attr_writer :meow
+
+  def initilize(name)
+    @name = name
+    
+  end
+
+  def meow
+    puts "meow!"
+  end
+
+kitty = Cat.new
+kitty.name = "maru"
+
+end
